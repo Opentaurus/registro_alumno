366e4ae035754f28bd9fe0a1309875df74ee65e5
Pascual Pérez Romero <pascual.perez@exitus.com>
2021-05-23 15:07:59 -0500

    Iniciando proyecto Maven con Sprint

---
+++ registro-alumno-backend-java/registroalumno/src/main/java/com/api/registroalumno/RegistroalumnoApplication.java
@@ -0,0 +1,13 @@
+package com.api.registroalumno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroalumnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroalumnoApplication.class, args);
+	}
+
+}
---
+++ registro-alumno-backend-java/registroalumno/src/test/java/com/api/registroalumno/RegistroalumnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.registroalumno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroalumnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
