e885a0b4c8730a4e8e0866bfd116ab836587c50d
Pascual Pérez Romero <pascual.perez@exitus.com>
2021-05-23 21:58:33 -0500

    Agregando los modulos home y login con sus respectivos componentes, ambos apuntados en el routing

--- registro-alumno-frontend-angular/registroalumnoapp/src/app/app.module.ts
+++ registro-alumno-frontend-angular/registroalumnoapp/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { HttpClientModule } from '@angular/common/http';
   declarations: [
     AppComponent,
     MainComponent,
-    OtherComponent,
+    OtherComponent
   ],  
   exports: [
     CommonModule,        
--- registro-alumno-frontend-angular/registroalumnoapp/src/app/app.routing.ts
+++ registro-alumno-frontend-angular/registroalumnoapp/src/app/app.routing.ts
@@ -5,13 +5,21 @@
  import { Routes } from '@angular/router';
  import { MainComponent } from './@registroalumno/shared/main/main.component';
  import { OtherComponent } from './@registroalumno/shared/other/other.component';
-  
+
  export const AppRoutes: Routes = [
    {
      path: 'registroalumno',
      component: MainComponent,
      children: [
-       /*El home*/
+      {
+        path: 'home',
+      loadChildren: () => import('./@registroalumno/pages/home/home.module').then(m => m.HomeModule),
+      },        
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+      }
      ]
    },
    {
@@ -19,7 +27,8 @@
      component: OtherComponent,
      children: [
        {
-        /*El login*/  
+        path: 'login',
+        loadChildren: () => import('./@registroalumno/pages/login/login.module').then(m => m.LoginModule),       
        }
      ]
    },
